0bfa5eed2e3eefdf3e1fbebac4c2149a3becdcdc
Pavel Litvinenko <korvine1980@yandex.ru>
2025-01-05 11:09:04 +0500

    3. Удалить head в односвязном списке. [#51424]

---
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -0,0 +1,85 @@
+package ru.job4j.collection;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class ForwardLinked<T> implements Iterable<T> {
+    private int size;
+    private int modCount;
+    private Node<T> head;
+
+    public void add(T value) {
+        ForwardLinked.Node<T> newNode = new ForwardLinked.Node<>(value, null);
+        if (head == null) {
+            head = newNode;
+        } else {
+            node(size - 1).next = newNode;
+        }
+        size++;
+        modCount++;
+    }
+
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        return node(index).item;
+    }
+
+    public T deleteFirst() {
+        if (head == null) {
+            throw new NoSuchElementException();
+        }
+        Node<T> nextHead = head.next;
+        T result = head.item;
+        head.item = null;
+        head = nextHead;
+        size--;
+        modCount++;
+        return result;
+    }
+
+    private ForwardLinked.Node<T> node(int index) {
+        ForwardLinked.Node<T> node = head;
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+        return node;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<>() {
+            private final int mod = modCount;
+            private ForwardLinked.Node<T> next = head;
+
+            @Override
+            public boolean hasNext() {
+                if (mod != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return next != null;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                T current = next.item;
+                next = next.next;
+                return current;
+            }
+        };
+    }
+
+    private static class Node<T> {
+        private T item;
+        private ForwardLinked.Node<T> next;
+
+        Node(T element, ForwardLinked.Node<T> next) {
+            this.item = element;
+            this.next = next;
+        }
+    }
+}
