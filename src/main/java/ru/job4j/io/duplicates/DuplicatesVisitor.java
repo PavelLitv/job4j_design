b61c257d6422b04bdada22ef285fb6ae26b1a391
Pavel Litvinenko <korvine1980@yandex.ru>
2025-01-16 20:42:13 +0500

    11. Поиск дубликатов [#315066]

---
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -0,0 +1,33 @@
+package ru.job4j.io.duplicates;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+
+public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+    HashMap<FileProperty, String> fileProperties = new HashMap<>();
+
+    @Override
+    public FileVisitResult visitFile(Path file,
+                                     BasicFileAttributes attributes) throws IOException {
+        if (attributes.isRegularFile()) {
+            FileProperty fileProperty = new FileProperty(attributes.size(), file.getFileName().toString());
+            fileProperties.compute(fileProperty, (k, v) ->
+                    v == null ? file.toAbsolutePath().toString() : v + System.lineSeparator() + file.toAbsolutePath());
+        }
+        return super.visitFile(file, attributes);
+    }
+
+    public void printDuplicates() {
+        fileProperties.forEach((key, value) -> {
+            if (value.contains(System.lineSeparator())) {
+                System.out.printf("%s - %.1f Mb%n", key.getName(), key.getSize() / 1024.0);
+                System.out.println(value);
+                System.out.println();
+            }
+        });
+    }
+}
