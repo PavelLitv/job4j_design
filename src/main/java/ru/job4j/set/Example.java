6847cd555c96455328b3a7a5b566b49c7737b9c9
Pavel Litvinenko <korvine1980@yandex.ru>
2025-06-08 13:20:51 +0500

    add counter for Example.java

--- src/main/java/ru/job4j/set/Example.java
+++ src/main/java/ru/job4j/set/Example.java
@@ -1,5 +1,18 @@
 package ru.job4j.set;
 
 public class Example {
-    int count = 1;
+    private static int count = 0;
+
+    public Example() {
+        System.out.println("Example #" + ++count);
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Example example = new Example();
+        System.out.println(getCount());
+    }
 }
